cb8d3a3ce7d5f5338de06864382e36d4dd9be3fd
Ezra Leye <ezraleyeee@gmail.com>
2026-01-17 20:49:56 +0300

    feat(validation): add index validation schema

---
+++ backend/src/validations/index.js
@@ -0,0 +1,10 @@
+/**
+ * Validation Schemas Index
+ * Export all validation schemas
+ */
+
+export * from './authValidation.js';
+export * from './userValidation.js';
+export * from './productValidation.js';
+export * from './cartValidation.js';
+export * from './orderValidation.js';
